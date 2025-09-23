792a794370d1cc4ed8d47196c9a5a487fbb56e4c
Tapiwanashe Nicole Ciriboto <ciribotonicole@gmail.com>
2025-09-23 11:26:30 +0200

    Updated Resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -43,7 +43,7 @@ export const About = () => {
                 Get in Touch
               </a>
               <a
-                href="/NICOLE CIRIBOTO.Resume.pdf"
+                href="/NICOLE CIRIBOTO RESUME.pdf"
                 target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
